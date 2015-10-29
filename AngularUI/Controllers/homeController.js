3774e14b27ea45e5275b2350c78179c68e8320bb
Ekta Jha <ektajha@amazon.com>
2015-10-30 02:03:06 +0530

    Sample Application

---
+++ AngularUI/Controllers/homeController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    var app = angular.module("myapp");
+
+    var homeController = function ($scope, extractService) {
+        $scope.message = "This is home view"
+        var onsuccess = function (data) {
+            alert('Inside success');
+            alert(data);
+            $scope.data = data;
+            //important to parse
+            var jsonObject = JSON.parse(data);
+            alert(jsonObject);
+            alert(jsonObject[0].Id);
+            alert(data[0])
+            $scope.id = jsonObject[0].Name;
+        };
+
+        var onError = function () {
+            $scope.error = "something went wronge";
+
+        };
+       
+        extractService.getData().then(onsuccess,onError);
+        
+   };
+    
+
+    app.controller("homeController", ["$scope", "extractService", homeController]);
+
+}());
---
+++ AngularUI/Services.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    var app = angular.module("myapp");
+
+    var extractService = function ($http) {
+
+        function getData() {
+            return $http.get("http://localhost:58306/api/Extract")
+                     .then(function (response) {
+                         return response.data;
+                     });
+        };
+
+        return {
+            getData: getData
+        };
+
+    };
+
+    app.factory("extractService", ["$http", extractService]);
+}());
